5d4872782742fd3ad0769db7c5fcdf2fe2541fed
Aracely Alba <aracelyalba8@gmail.com>
2023-03-10 14:28:43 -0430

    Added scrollbar color

--- src/services/getCoins.jsx
+++ src/services/getCoins.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export function getCoins() {
 
-  const API = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1`;
+  const API = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1`;
 
   
   return axios.get(API)
